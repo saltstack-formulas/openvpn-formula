f4b25318a96a663bef1af3a18fef1137165330ff
Dafydd Jones <dafydd@techneg.it>
2017-08-25 17:23:11 +0100

    do not use a default prefix, allow user to set full adapter name

--- openvpn/files/tap-adapter.ps1
+++ openvpn/files/tap-adapter.ps1
@@ -8,7 +8,7 @@ $tapInstallCmd = "$tapDir\bin\tapinstall.exe"
 $tapInstallArgs = "install", "`"$tapDir\driver\OemVista.inf`"", "tap0901"     # Backtick quotes https://stackoverflow.com/q/17550663
 
 if ($PSCmdlet.ParameterSetName -eq 'new') {
-    if (Get-NetAdapter -InterfaceDescription $tapDesc | Where-Object Name -eq ovpn-$New) { 
+    if (Get-NetAdapter -InterfaceDescription $tapDesc | Where-Object Name -eq $New) {
         Write-Host "changed=no comment=`'TAP-Windows adapter $New exists`'"
         exit
     }
@@ -16,13 +16,13 @@ if ($PSCmdlet.ParameterSetName -eq 'new') {
         $p = Start-Process $tapInstallCmd -ArgumentList $tapInstallArgs -NoNewWindow -Wait -PassThru
     }
     Get-NetAdapter -InterfaceDescription $tapDesc | Where-Object Name -Like Eth* `
-        | Select-Object -First 1 | Rename-NetAdapter -NewName ovpn-$New
+        | Select-Object -First 1 | Rename-NetAdapter -NewName $New
     Write-Host "changed=yes comment=`'TAP-Windows adapter $New created`'"
 }
 
 if ($PSCmdlet.ParameterSetName -eq 'remove') {
     # No practical way to remove individual TAP adapter, so rename randomly to get it "out of the way"
-    Get-NetAdapter -Name ovpn-$Remove | Rename-NetAdapter -NewName "tmp$(Get-Random -Min 10 -Max 999)"   
+    Get-NetAdapter -Name $Remove | Rename-NetAdapter -NewName "tmp$(Get-Random -Min 10 -Max 999)"
     Write-Host "changed=yes comment=`'TAP-Windows adapter $Remove removed`'"
 }
 
