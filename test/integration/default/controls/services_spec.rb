1e1719d84d7149dd3d83e07d8331dce55b9d8398
Alexander Weidinger <aw@sz9i.net>
2019-07-13 18:19:26 +0200

    ci(cleanup): dropped obsolete opensuse-leap-15 workaround

--- test/integration/default/controls/services_spec.rb
+++ test/integration/default/controls/services_spec.rb
@@ -1,43 +1,36 @@
-# Temporary `if` due to `opensuse-leap-15` bug re: `service`
-if os[:name] == 'suse'
-  puts "[Skip `service`-based tests due to `opensuse-leap-15` detection bug (see https://github.com/inspec/train/issues/377)]"
-else
+control 'OpenVPN service' do
+  impact 0.5
+  title 'should be running and enabled'
 
-  control 'OpenVPN service' do
-    impact 0.5
-    title 'should be running and enabled'
-
-    # single service
-    if os[:name] == 'centos' and os[:release].start_with?('6')
-        describe service("openvpn") do
-          it { should be_enabled }
-          it { should be_running }
-        end
-
-    # multiple services
-    else
-      %w(server client).each do |role|
-
-        prefix = case os[:name]
-        when 'fedora' then "openvpn-#{role}"
-        else 'openvpn'
-        end
-
-        describe service("#{prefix}@my#{role}1.service") do
-          it { should be_enabled }
-          it { should be_running }
-        end
+  # single service
+  if os[:name] == 'centos' and os[:release].start_with?('6')
+      describe service("openvpn") do
+        it { should be_enabled }
+        it { should be_running }
       end
-    end
 
+  # multiple services
+  else
     %w(server client).each do |role|
-      logfile = "/var/log/openvpn/my#{role}1.log"
 
-      describe command("sh -c 'for i in $(seq 1 60); do if grep \"Initialization Sequence Completed\" #{logfile}; then exit 0; fi; echo -n '.'; sleep 1; done; cat #{logfile}; exit 1'") do
-        its('exit_status') { should be 0 }
-        its('stdout') { should include "Initialization Sequence Completed" }
+      prefix = case os[:name]
+      when 'fedora' then "openvpn-#{role}"
+      else 'openvpn'
+      end
+
+      describe service("#{prefix}@my#{role}1.service") do
+        it { should be_enabled }
+        it { should be_running }
       end
     end
   end
 
+  %w(server client).each do |role|
+    logfile = "/var/log/openvpn/my#{role}1.log"
+
+    describe command("sh -c 'for i in $(seq 1 60); do if grep \"Initialization Sequence Completed\" #{logfile}; then exit 0; fi; echo -n '.'; sleep 1; done; cat #{logfile}; exit 1'") do
+      its('exit_status') { should be 0 }
+      its('stdout') { should include "Initialization Sequence Completed" }
+    end
+  end
 end
